f64c561ef554816c636dc4648c228f3e21a6bd8a
zhangtianli2006 <zhangtianli2006@163.com>
2021-10-13 13:23:25 +0800

    [test] new merge.py
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- tests/merge.py
+++ tests/merge.py
@@ -1,60 +1,22 @@
-import os
 import cv2
-
-import numpy as np
+import os
 
 cv2.ocl.setUseOpenCL(False)
 
-img_dir = "img_merge_test_easy"  # 原始文件目录
-names = os.listdir(img_dir)
+img_dir = "img_merge_test"
+output = "result.jpg"
+conf_path = "img_merge_test/files.conf"
 
+fp = open(conf_path, "r")
+filenames = [each.rstrip("\r\n") for each in fp.readlines()]
+print(filenames)
 
 images = []
-for name in names:
-    img_path = os.path.join(img_dir, name)
-    image = cv2.imread(img_path)
-
-    images.append(image)
-
-# for i, img in enumerate(images):
-#     cv2.imshow(f"img {i}", img)
+for img_name in filenames:
+    img = cv2.imread(os.path.join(img_dir, img_name))
+    images.append(img)
 
 stitcher = cv2.Stitcher_create()
-status, stitched = stitcher.stitch(images)
+status, res = stitcher.stitch(images)
 
-# 四周填充黑色像素，再得到阈值图
-stitched = cv2.copyMakeBorder(stitched, 10, 10, 10, 10, cv2.BORDER_CONSTANT, (0, 0, 0))
-gray = cv2.cvtColor(stitched, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
-
-cnts, hierarchy = cv2.findContours(
-    thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-)
-cnt = max(cnts, key=cv2.contourArea)
-
-mask = np.zeros(thresh.shape, dtype="uint8")
-x, y, w, h = cv2.boundingRect(cnt)
-cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
-
-minRect = mask.copy()
-sub = mask.copy()
-
-# 开始while循环，直到sub中不再有前景像素
-while cv2.countNonZero(sub) > 0:
-    minRect = cv2.erode(minRect, None)
-    sub = cv2.subtract(minRect, thresh)
-
-cnts, hierarchy = cv2.findContours(
-    minRect.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-)
-cnt = max(cnts, key=cv2.contourArea)
-x, y, w, h = cv2.boundingRect(cnt)
-
-# 使用边界框坐标提取最终的全景图
-# stitched = stitched[y : y + h, x : x + w]
-
-# cv2.imshow("res", stitched)
-cv2.imwrite('final.jpg', stitched)
-
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imwrite(os.path.join(img_dir, output), res)
