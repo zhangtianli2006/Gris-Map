3d0b6f8e3fe10a6a8b158f338956f38efdf2d1b0
zhangtianli2006 <zhangtianli2006@163.com>
2021-10-16 11:05:15 +0800

    [test] raw 2d stitch test done
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- tests/img/brute_merge/merge.py
+++ tests/img/brute_merge/merge.py
@@ -1,14 +1,24 @@
 import numpy as np
 import cv2
 
+delta_x = 0.0
+delta_y = 638.0
+
 img1 = cv2.imread("1.png")
-# img2 = cv2.imread("2.png")
+img2 = cv2.imread("2.png")
 
-rows, cols = img1.shape[0:2]
-M = np.float32([[1, 0, 100], [0, 1, 50]])
-dst = cv2.warpAffine(img1, M, (cols, rows))
+img3 = cv2.copyMakeBorder(img2, 0, 4000, 0, 4000, cv2.BORDER_CONSTANT)
+# cv2.imwrite("border.png", img3);
 
-# cv2.imwrite("move.png", dst)
+tot_rows = img2.shape[0] + img1.shape[0] - delta_x
+tot_cols = img2.shape[1] + img1.shape[1] - delta_y
 
-cv2.imwrite("move.png", dst)
-# img2[img2.shape[0]:img2.shape[0] + img1.shape[0], img2.shape[1]:img2.shape[1] + img1.shape[1]] = img1
+# res = np.matrix()
+
+for i in range(0, img1.shape[0]):
+    for j in range(0, img1.shape[1]):
+        img3[int(delta_y + i)][int(delta_x + j)] = img1[i][j]
+
+cv2.imwrite("resault.png", img3);
+
+# print(len(img2.tolist()))
